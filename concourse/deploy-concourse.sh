7efd8a849313bd2dd1c158e33daf1f806023081c
aguestuser <aguestuser@riseup.net>
2016-04-14 22:48:51 -0400

    [#1] AG | Tweak concourse config
    * Make worker a t2.micro instead of m2.medium
    * Make concourse network public

--- concourse/deploy-concourse.sh
+++ concourse/deploy-concourse.sh
@@ -5,10 +5,10 @@ pushd
 source ~/.bashrc
 cd ${WHEREAT_ROOT}/whereat-ci/concourse/
 
-echo 'Uploading releases to bosh director...'
-bosh upload release https://github.com/concourse/concourse/releases/download/v1.0.0/concourse-1.0.0.tgz &&
-    bosh upload release https://github.com/concourse/concourse/releases/download/v1.0.0/garden-linux-0.335.0.tgz &&
-    bosh releases
+# echo 'Uploading releases to bosh director...'
+# bosh upload release https://github.com/concourse/concourse/releases/download/v1.0.0/concourse-1.0.0.tgz &&
+#     bosh upload release https://github.com/concourse/concourse/releases/download/v1.0.0/garden-linux-0.335.0.tgz &&
+#     bosh releases
 
 echo 'Creating concourse manifest...'
 cat concourse-dist.yml > concourse.yml
